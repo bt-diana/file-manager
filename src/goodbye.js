df3e418a77e1bbf143c7f34cee7635a21f090604
Diana Butiakova <butiakova.diana@gmail.com>
2025-05-04 20:18:39 +0600

    feat: print gooodbye message on start script end

---
+++ src/goodbye.js
@@ -0,0 +1,7 @@
+const { USERNAME } = require('./constants.js');
+
+const goodbye = () => {
+    console.log(`Thank you for using File Manager, ${USERNAME}, goodbye!`)
+};
+
+module.exports = goodbye;
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 const hello = require('./hello.js');
+const goodbye = require('./goodbye.js');
 
 hello();
+goodbye();
