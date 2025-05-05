5bf2fbbd9178205f5c86537b8cfdb0aba8358ee4
Diana Butiakova <butiakova.diana@gmail.com>
2025-05-06 00:18:52 +0600

    feat: add ls command

--- src/runCommand.js
+++ src/runCommand.js
@@ -1,6 +1,7 @@
 const { InvalidInput } = require('./errors.js');
 const up = require('./commands/up.js');
 const cd = require('./commands/cd.js');
+const ls = require('./commands/ls.js');
 
 const runCommand = async (command, options) => {
     if (command === 'up' && options.length === 0) {
@@ -9,6 +10,10 @@ const runCommand = async (command, options) => {
 
     if (command === 'cd' && options.length === 1) {
         return cd(options[0]);
+    }
+
+    if (command === 'ls' && options.length === 0) {
+        return ls();
     }
 
     throw new InvalidInput();
