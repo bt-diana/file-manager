07abf17a0b44481c85df5d8b9940b09e4d0bed9d
Diana Butiakova <butiakova.diana@gmail.com>
2025-05-06 02:19:26 +0600

    feat: create add command

---
+++ src/commands/add.js
@@ -0,0 +1,23 @@
+import { getCurrentDir } from '../currentDir.js';
+import { InvalidInput, OperationFailed } from '../errors.js';
+import { resolve, parse } from 'node:path';
+import { open } from 'node:fs/promises';
+
+const add = async (fileName) => {
+    const filePath = resolve(getCurrentDir(), fileName);
+
+    let file;
+    try {
+        file = await open(filePath, 'wx');
+        console.log(`Created a new empty file ${filePath}`)
+    } catch (e) {
+        if (e.code === 'EEXIST') {
+            throw new InvalidInput();
+        }
+        throw new OperationFailed();
+    } finally {
+        await file?.close();
+    }
+};
+
+export default add;
--- src/runCommand.js
+++ src/runCommand.js
@@ -3,6 +3,7 @@ import up from './commands/up.js';
 import cd from './commands/cd.js';
 import ls from './commands/ls.js';
 import cat from './commands/cat.js';
+import add from './commands/add.js';
 
 const runCommand = async (command, options) => {
     if (command === 'up' && options.length === 0) {
@@ -19,6 +20,10 @@ const runCommand = async (command, options) => {
 
     if (command === 'cat' && options.length === 1) {
         return cat(options[0]);
+    }
+
+    if (command === 'add' && options.length === 1) {
+        return add(options[0]);
     }
 
     throw new InvalidInput();
