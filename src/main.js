029fe76612102270c34ce68ad08e033fd7f725a9
Diana Butiakova <butiakova.diana@gmail.com>
2025-05-05 21:36:05 +0600

    feat: wait for the user to input command

--- src/main.js
+++ src/main.js
@@ -1,10 +1,33 @@
+const { createInterface } = require('node:readline/promises');
 const { USERNAME } = require('./constants.js');
 const { currentDirectory } = require('./variables.js');
 
 const main = () => {
+    const rl = createInterface(process.stdin, process.stdout);
+
     console.log(`Welcome to the File Manager, ${USERNAME}!`);
     console.log(`You are currently in ${currentDirectory}`);
-    console.log(`Thank you for using File Manager, ${USERNAME}, goodbye!`);
+    console.log('Please, enter your command:');
+
+    rl.on('line', (input) => {
+        if (input === 'exit' || input === '.exit') {
+            rl.emit('SIGINT');
+        } else {
+            rl.pause();
+            // TO-DO: process command
+            rl.resume();
+        }
+    });
+
+    rl.on('resume', () => {
+        console.log(`You are currently in ${currentDirectory}`);
+        console.log('Please, enter your command:');
+    }); 
+
+    rl.on('SIGINT', () => {
+        console.log(`Thank you for using File Manager, ${USERNAME}, goodbye!`);
+        rl.close();
+    });
 };
 
 main();
